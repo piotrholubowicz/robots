41c2c647d8a1a8367a51e9e97213dbb60596cc75
Piotr Holubowicz <piotrekhol@gmail.com>
2016-12-18 14:38:34 -0800

    ai should work

--- app/model/ai.ts
+++ app/model/ai.ts
@@ -1,5 +1,6 @@
 import { Game } from './game';
-import { Coords } from './common';
+import { Piece, Side, Type } from './piece';
+import { Coords, Utils } from './common';
 
 export class AI {
     depth: number;
@@ -8,13 +9,52 @@ export class AI {
     }
 
     nextMove(game: Game): Move {
-        return undefined;
+        let moves = this.getAllMoves(game);
+        for (let move of moves) {
+            this.evaluate(move, game);
+        }
+        Utils.shuffleInPlace(moves);
+        moves.sort((m1, m2) => m2.value-m1.value);
+        return moves[0];
+    }
+
+    getAllMoves(game: Game): Move[] {
+        let moves: Move[] = [];
+        for (let i=0; i<game.pieces.length; i++) {
+            for (let j=0; j<game.pieces[i].length; j++) {
+                let piece = game.pieces[i][j];
+                for (let pos of piece.movements()) {
+                    let x2 = i + pos[0];
+                    let y2 = j + pos[1];
+                    if (game.isOutOfBounds(x2, y2)) {
+                        continue;
+                    }
+                    let otherPiece = game.pieces[x2][y2];
+                    if (otherPiece.side == Side.NONE) {
+                        // move
+                        moves.push(new Move(new Coords(i,j), new Coords(x2,y2)));
+                    } else if (otherPiece.side == piece.side) {
+                        // nothing
+                    } else {
+                        // capture
+                        moves.push(new Move(new Coords(i,j), new Coords(x2,y2)));
+                    }
+                }
+            }
+        }
+        return moves;
+    }
+
+    evaluate(move: Move, game: Game) {
+        // TODO do something better
+        move.value = 0.5;
     }
 }
 
 export class Move {
     src: Coords;
     dst: Coords;
+    value: number;
 
     constructor(src: Coords, dst: Coords) {
         this.src = src;
--- app/model/common.ts
+++ app/model/common.ts
@@ -7,3 +7,27 @@ export class Coords {
         this.y = y;
     }
 }
+
+export class Utils {
+    static getRandom(i: number, j: number): number {
+        return i + Math.floor(Math.random() * j);
+    }
+
+    static shuffleInPlace<T>(array: T[]): T[] {
+        // if it's 1 or 0 items, just return
+        if (array.length <= 1) return array;
+
+        // For each index in array
+        for (let i = 0; i < array.length; i++) {
+
+            // choose a random not-yet-placed item to place there
+            // must be an item AFTER the current item, because the stuff
+            // before has all already been placed
+            const randomChoiceIndex = this.getRandom(i, array.length);
+
+            // place our random choice in the spot by swapping
+            [array[i], array[randomChoiceIndex]] = [array[randomChoiceIndex], array[i]];
+        }
+        return array;
+    }
+}
